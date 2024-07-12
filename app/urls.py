f7953292c10dc7b0df6e9709f6a8f425664663c7
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-07-13 00:49:21 +0900

    createb post views, post model, migrations, and templates

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 
 urlpatterns = [
     path("echo/", views.echo_page),
+    path("liveblog", views.liveblog_index),
+    path("liveblog/post/<int:post_id>", views.post_partial, name="post-partial"),
 ]
