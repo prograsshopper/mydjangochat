de1a5d1ee62a04e3171f5c91ad854aa6412db982
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-09-22 18:00:12 +0900

    modify parent class: Websocketconsumer -> Json based

--- app/consumers.py
+++ app/consumers.py
@@ -1,35 +1,31 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class LiveblogConsumer(WebsocketConsumer):
+class LiveblogConsumer(JsonWebsocketConsumer):
     groups = ["liveblog"]
 
     def liveblog_post_created(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveblog_post_updated(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveblog_post_deleted(self, event_dict):
-        self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
 
-class EchoConsumer(WebsocketConsumer):
+class EchoConsumer(JsonWebsocketConsumer):
     '''
     웹소켓 클라이언트에서 text_frame으로 보내면, text data 인자에 담겨 호출
     binary frame에 담아 보내면 bytes data 에 담겨 호출된다
     ref: javascript websocket
     '''
-    def receive(self, text_data=None, bytes_data=None):
-        obj = json.loads(text_data)
-        print(f"수신: {obj}")
-
-        json_string = json.dumps({
-            "content": obj["content"],
-            "user": obj["user"]
+    def receive(self, content=None, **kwargs):
+        print(f"수신: {content}")
+        self.send_json({
+            "content": content["content"],
+            "user": content["user"]
         })
-        self.send(json_string)
         # self.send(f"You said: {text_data}")
-
