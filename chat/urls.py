e3bdc7f2ce7811881b8edb9dedc72df8c7f2a4f9
prograsshopper <16264413+prograsshopper@users.noreply.github.com>
2024-10-01 15:42:46 +0900

    real chat service: 2 - group chatting through square channel

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,5 @@ app_name = "chat"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/<str:room_name>/chat/", views.room_chat, name="room_chat"),
+    path("<str:room_name>/chat/", views.room_chat, name="room_chat"),
 ]
